2c7f1518403e8a156cb9f3831b45d1447d9b8011
SajjadMollah <sajjadmollah314@gmail.com>
2024-11-03 11:16:36 +0000

    testing error

--- simple_server/tests/test_app.py
+++ simple_server/tests/test_app.py
@@ -2,3 +2,10 @@ def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Hello, world!"
+
+
+def test_get_home_1(web_client):
+    response = web_client.get("/1")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "Hello, world!"
+
